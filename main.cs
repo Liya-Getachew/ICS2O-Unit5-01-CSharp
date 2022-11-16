6a5c8365ea9c5003688bab0b79b68adfb43a39a4
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-11-16 02:19:44 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -10,8 +10,8 @@ class Program
     public static void Main(string[] args)
     {
         // This function generates a random number from 1-6
-        Random random = new System.Random(); 
-        int randomNumber = random.Next(1,6);
+        Random random = new System.Random();
+        int randomNumber = random.Next(1, 6);
 
         int valueFromInput;
 
@@ -21,14 +21,14 @@ class Program
         Console.Write("insert guess here: ");
         valueFromInput = Convert.ToInt32(Console.ReadLine());
 
-        if ( valueFromInput == randomNumber) 
+        if (valueFromInput == randomNumber) 
         {
-          Console.WriteLine("You'reeee....RIGHT! The answer was " + randomNumber + ". ");
+        Console.WriteLine("You'reeee....RIGHT! The answer was " + randomNumber + ". ");
         }
 
-        if (valueFromInput != randomNumber) 
+        if (valueFromInput != randomNumber)
         {
-          Console.WriteLine("You'reeee....WRONG! Try again.");
+        Console.WriteLine("You'reeee....WRONG! Try again.");
         }
 
         Console.WriteLine("\nDone.");
